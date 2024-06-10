832fde2549b80a7c575de3281978b0088227ac59
unknown <you@example.comjdiegocm@hotmail.com>
2024-06-10 16:57:50 -0500

    vista colculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,11 +11,16 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, FormsModule],
 })
 export class HomePage implements OnInit {
-  
+  //ver: String = "0";
+  num: String = "";
 
   constructor() {}
 
   ngOnInit() {
+  }
+
+  numeros(){
+    //this.ver= "num" + this.num;
   }
   
 }
